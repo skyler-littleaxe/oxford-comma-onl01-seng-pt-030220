bbeece8578e05a0ecc7036fe48fc230b9f7328af
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-11 01:20:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,10 @@ def oxford_comma(array)
   array.join
   elsif array.size == 2
     array.join(" and ")
-    elsif array.size >= 3
+    elsif array.size == 3
     array[0..1].join(", ")+", and "+array[2]
+  else array.size > 3
+    array[0..]
     
   
   
